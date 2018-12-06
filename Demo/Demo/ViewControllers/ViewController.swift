8e914d59e59689a0a0e77457cc0904e6f29d4078
Max <sibo_jian_29903549@163.com>
2018-12-06 22:52:37 +0800

    [功能]($\Demo & $\Sources)
    添加 Cocoa Touch Framework 项目主体，对项目文件目录结构调整。
    添加 Single View App 项目主体，对项目文件目录结构调整。
    添加 JSSegmentControl.xcworkspace 管理上述项目主体。

---
+++ Demo/Demo/ViewControllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  JSSegmentControl-Demo
+//
+//  Created by Max on 2018/12/6.
+//  Copyright © 2018 Max. All rights reserved.
+//
+
+import UIKit
+import JSSegmentControl
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Sources/JSSegmentControl.h
@@ -0,0 +1,19 @@
+//
+//  JSSegmentControl.h
+//  JSSegmentControl
+//
+//  Created by Max on 2018/12/6.
+//  Copyright © 2018 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSSegmentControl.
+FOUNDATION_EXPORT double JSSegmentControlVersionNumber;
+
+//! Project version string for JSSegmentControl.
+FOUNDATION_EXPORT const unsigned char JSSegmentControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSSegmentControl/PublicHeader.h>
+
+
