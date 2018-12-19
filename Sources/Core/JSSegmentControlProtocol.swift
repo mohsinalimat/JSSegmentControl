74a1eab13d19748d6e3c335936db02978b5e9872
Max <sibo_jian_29903549@163.com>
2018-12-19 22:13:47 +0800

    [功能]($JSSegmentControlProtocol)
    `JSTitleDataSource` & `JSTitleDelegate` 增加 `@objc` 前缀。

--- Sources/Core/JSSegmentControlProtocol.swift
+++ Sources/Core/JSSegmentControlProtocol.swift
@@ -8,22 +8,22 @@
 
 import Foundation
 
-public protocol JSTitleDataSource: NSObjectProtocol {
-    func numberOfTitles() -> Int
-    func title(_ title: JSTitleView, containerAt index: Int) -> JSTitleContainerView
 }
 
-public protocol JSTitleDelegate: NSObjectProtocol {
-    func title(_ title: JSTitleView, didSelectAt index: Int)
-    func title(_ title: JSTitleView, didDeselectAt index: Int)
 }
 
 public protocol JSContentDataSource: NSObjectProtocol {
     func numberOfContents() -> Int
     func content(_ content: JSContentView, containerAt index: Int) -> UIViewController
+@objc public protocol JSTitleDataSource: NSObjectProtocol {
+    @objc func numberOfTitles() -> Int
+    @objc func title(_ title: JSTitleView, containerAt index: Int) -> JSTitleContainerView
 }
 
 public protocol JSContentDelegate: NSObjectProtocol {
     func content(_ content: JSContentView, didSelectAt index: Int)
     func content(_ content: JSContentView, didDeselectAt index: Int)
+@objc public protocol JSTitleDelegate: NSObjectProtocol {
+    @objc func title(_ title: JSTitleView, didSelectAt index: Int)
+    @objc func title(_ title: JSTitleView, didDeselectAt index: Int)
 }
